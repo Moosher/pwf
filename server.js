38c1e174981fc9b0f1acacc5ba2b7ab65384b55c
Fernando Moosher Felix <fernando.felix@vectorinf.com.br>
2019-12-11 12:08:45 -0300

    updates to deploy

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-app.use('/', express.static(path.join(__dirname, 'dist/example')));
-const PORT = process.env.PORT || 8080;
-const IP = process.env.IP || '0.0.0.0';
-app.listen(PORT, IP, (error) => {
-    if(error)
-        return console.log(`[ FAIL ] Erro ao subir servidor: ${error}`);
-    return console.log(`[  OK  ] Servidor iniciado em ${IP}:${PORT}`);
-});
-// Install express server
-// const express = require('express');
-// const path = require('path');
-
+// const express = require("express");
+// const path = require("path");
 // const app = express();
-
-// // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist/<name-of-app>'));
-
-// app.get('/*', function(req,res) {
-    
-// res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+// app.use('/', express.static(path.join(__dirname, 'dist/PWF')));
+// const PORT = process.env.PORT || 8080;
+// const IP = process.env.IP || '0.0.0.0';
+// app.listen(PORT, IP, (error) => {
+//     if(error)
+//         return console.log(`[ FAIL ] Erro ao subir servidor: ${error}`);
+//     return console.log(`[  OK  ] Servidor iniciado em ${IP}:${PORT}`);
 // });
+// Install express server
+const express = require('express');
+const path = require('path');
 
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/PWF'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/PWF/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
